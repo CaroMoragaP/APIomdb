c2bc06c3f5845e20fa1c3ec85e999c50377706c5
C.MORAGA <cmoragap@a.mikeldi.com>
2025-11-21 10:18:37 +0100

    proyecto inicial

---
+++ src/main/java/com/apiomdb/demo/ApIomdbApplication.java
@@ -0,0 +1,13 @@
+package com.apiomdb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIomdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIomdbApplication.class, args);
+	}
+
+}
